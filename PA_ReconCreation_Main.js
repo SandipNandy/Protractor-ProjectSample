6c298c1bea8e7af21209b3215669ccaaa3fca010
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-01 09:10:23 +0530

    1st commit
    Directory has been changed now

--- PA_ReconCreation_Main.js
+++ PA_ReconCreation_Main.js
@@ -17,7 +17,7 @@
 var path = require('path');
 var XLSX = require('xlsx');
 var workbook = XLSX.readFile('C:/Users/SandipNandi/Project/SingleTouchAutomationAvnet/ReconCore/Automation_PassFail_StampingAvnet.xlsx');
-var workbook1 = XLSX.readFile('C:/Users/SandipNandi/Project/SingleTouchAutomationAvnet/ReconCore/R2R Data Template for Rec Planning_Avnet.xlsx');
+var workbook1 = XLSX.readFile('../ReconCore/R2R Data Template for Rec Planning_Avnet.xlsx');
 
 var WorksheetBDD = workbook1.Sheets['BDD_Framework'];
 var WorksheetControlstatement = workbook1.Sheets['Control_Statements'];
